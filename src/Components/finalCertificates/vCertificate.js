f4ea38181cc6de0350d4e32ba5eb5c07ff5307b7
ashraf-rahil-shaik <skashraf97@gmail.com>
2023-11-07 14:45:54 +0530

    changed date format

--- src/Components/finalCertificates/vCertificate.js
+++ src/Components/finalCertificates/vCertificate.js
@@ -98,7 +98,7 @@ function VCertificate() {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = String(date.getFullYear());
 
-    return `${day}-${month}-${year}`;
+    return `${month }-${day}-${year}`;
   };
 
 
